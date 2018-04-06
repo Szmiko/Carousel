d3df67514cc033b985081e422ad48cf0fa0205b1
Szymon Grabowski <szmiko@wp.pl>
2018-04-06 19:18:16 +0200

    Js problems

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-function start() {
+$(document).ready(function(){
 	console.log('DOM ready');
-	var carouselList = $('#carousel ul');
-	setInterval(changeSlides, 1000);
-	function changeSlides() {
-		carouselList.animate({'marginLeft':-500}, 500, moveFirstSlide);
+	
+	function start() {
+		var carouselList = $('#carousel ul');
+		setInterval(changeSlides, 1000);
+		function changeSlides() {
+			carouselList.animate({'marginLeft':-500}, 500, moveFirstSlide);
+		};
+		moveFirstSlide();
 	};
-	moveFirstSlide();
-};
+	start();
 
-start();
-
-function moveFirstSlide() {
-	var firstItem = carouselList.find('li:first');
-	var lastItem = carouselList.find('li:last');
-	lastItem.after(firstItem);
-	carouselList.css({marginLeft:0});
-};
+	function moveFirstSlide() {
+		var firstItem = carouselList.find('li:first');
+		var lastItem = carouselList.find('li:last');
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+	};
+});
 
